bb1558543d30b978a406a7835dc9aad400ab8085
gamallo <pablo.gamallo@gmail.com>
2020-12-07 22:40:11 +0100

    doc updated

--- doc/html_tutorial/images.pl
+++ doc/html_tutorial/images.pl
@@ -1,6 +1,24 @@
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate images original text with physical files.
 
+
+$key = q/((a&b)|c);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$((a\&amp;b)\vert c)$">|; 
+
+$key = q/((a|b)&c);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="$((a\vert b)\&amp;c)$">|; 
+
+$key = q/(a&(b|c));MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$(a\&amp;(b\vert c))$">|; 
 
 $key = q/(a&b)|(a&c);MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -14,77 +32,23 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img13.png"
  ALT="$(a\&amp;b)\vert(c)$">|; 
 
-$key = q/];MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="9" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$]$">|; 
-
-$key = q/nomath_inline}textexclamdownnomath_inline};MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="6" HEIGHT="21" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="\textexclamdown">|; 
-
-$key = q/(a|(b&c));MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img12.png"
- ALT="$(a\vert(b\&amp;c))$">|; 
-
-$key = q/(a&(b|c));MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img18.png"
- ALT="$(a\&amp;(b\vert c))$">|; 
-
-$key = q/((a&b)|c);MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img14.png"
- ALT="$((a\&amp;b)\vert c)$">|; 
-
-$key = q/[;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="9" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$[$">|; 
-
 $key = q/(a&c)|(b&c);MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="87" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img15.png"
  ALT="$(a\&amp;c)\vert(b\&amp;c)$">|; 
 
-$key = q/nomath_inline}textquestiondownnomath_inline};MSF=1.6;AAT/;
+$key = q/(a)|(b&c);MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="9" HEIGHT="21" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="\textquestiondown">|; 
+ WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$(a)\vert(b\&amp;c)$">|; 
 
-$key = q/=;MSF=1.6;AAT/;
+$key = q/(a|(b&c));MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="17" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img7.png"
- ALT="$=$">|; 
-
-$key = q/c;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="11" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img10.png"
- ALT="$c$">|; 
-
-$key = q/a;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img8.png"
- ALT="$a$">|; 
-
-$key = q/b;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="11" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img9.png"
- ALT="$b$">|; 
+ WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$(a\vert(b\&amp;c))$">|; 
 
 $key = q/+;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -98,17 +62,53 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img6.png"
  ALT="$-$">|; 
 
-$key = q/((a|b)&c);MSF=1.6;AAT/;
+$key = q/=;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img16.png"
- ALT="$((a\vert b)\&amp;c)$">|; 
+ WIDTH="17" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$=$">|; 
 
-$key = q/(a)|(b&c);MSF=1.6;AAT/;
+$key = q/[;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="68" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img11.png"
- ALT="$(a)\vert(b\&amp;c)$">|; 
+ WIDTH="9" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$[$">|; 
+
+$key = q/];MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="9" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$]$">|; 
+
+$key = q/a;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$a$">|; 
+
+$key = q/b;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$b$">|; 
+
+$key = q/c;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$c$">|; 
+
+$key = q/nomath_inline}textexclamdownnomath_inline};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="6" HEIGHT="21" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\textexclamdown">|; 
+
+$key = q/nomath_inline}textquestiondownnomath_inline};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="9" HEIGHT="21" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\textquestiondown">|; 
 
 1;
 
--- doc/html_tutorial/labels.pl
+++ doc/html_tutorial/labels.pl
@@ -1,14 +1,6 @@
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate labels original text with physical files.
 
-
-$key = q/sec:list_tags/;
-$external_labels{$key} = "$URL/" . q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:unicity/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
-$noresave{$key} = "$nosave";
 
 $key = q/sec:corr/;
 $external_labels{$key} = "$URL/" . q|node36.html|; 
@@ -18,31 +10,39 @@ $key = q/sec:dependencies/;
 $external_labels{$key} = "$URL/" . q|node4.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/sec:list_tags/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:unicity/;
+$external_labels{$key} = "$URL/" . q|node37.html|; 
+$noresave{$key} = "$nosave";
+
 1;
 
 
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # labels from external_latex_labels array.
 
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:corr/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:dependencies/;
+$external_latex_labels{$key} = q|1.2|; 
+$noresave{$key} = "$nosave";
 
 $key = q/sec:list_tags/;
 $external_latex_labels{$key} = q|1.3|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:verb/;
-$external_latex_labels{$key} = q|1.6|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:prp/;
-$external_latex_labels{$key} = q|1.10|; 
-$noresave{$key} = "$nosave";
-
 $key = q/sec:unicity/;
 $external_latex_labels{$key} = q|2.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:corr/;
-$external_latex_labels{$key} = q|2.2|; 
 $noresave{$key} = "$nosave";
 
 $key = q/tab:adj/;
@@ -53,16 +53,8 @@ $key = q/tab:adv/;
 $external_latex_labels{$key} = q|1.3|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:i/;
-$external_latex_labels{$key} = q|1.9|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:noun/;
-$external_latex_labels{$key} = q|1.5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:date/;
-$external_latex_labels{$key} = q|1.12|; 
+$key = q/tab:card/;
+$external_latex_labels{$key} = q|1.11|; 
 $noresave{$key} = "$nosave";
 
 $key = q/tab:conj/;
@@ -73,20 +65,28 @@ $key = q/tab:dt/;
 $external_latex_labels{$key} = q|1.4|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:pro/;
-$external_latex_labels{$key} = q|1.7|; 
+$key = q/tab:i/;
+$external_latex_labels{$key} = q|1.9|; 
 $noresave{$key} = "$nosave";
 
 $key = q/tab:marks/;
 $external_latex_labels{$key} = q|1.1|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:card/;
-$external_latex_labels{$key} = q|1.11|; 
+$key = q/tab:noun/;
+$external_latex_labels{$key} = q|1.5|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sec:dependencies/;
-$external_latex_labels{$key} = q|1.2|; 
+$key = q/tab:pro/;
+$external_latex_labels{$key} = q|1.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:prp/;
+$external_latex_labels{$key} = q|1.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:verb/;
+$external_latex_labels{$key} = q|1.6|; 
 $noresave{$key} = "$nosave";
 
 1;
